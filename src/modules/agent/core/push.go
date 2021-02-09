a008cdee998161555c72cbd8a66c9987f56dc848
dup2X <dup2snow@gmail.om>
2021-02-09 14:21:41 +0800

    MOD:进程和端口监控 上报到promethues

--- src/modules/agent/core/push.go
+++ src/modules/agent/core/push.go
@@ -4,12 +4,31 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
 	"github.com/toolkits/pkg/logger"
 
 	"github.com/dup2X/nightingale/src/common/dataobj"
 	"github.com/dup2X/nightingale/src/modules/agent/cache"
 	"github.com/dup2X/nightingale/src/modules/agent/config"
 )
+
+var (
+	gau     *prometheus.GaugeVec
+	gPusher *push.Pusher
+)
+
+func init() {
+	var prometheusReg = prometheus.NewRegistry()
+	gau = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "demo",
+		Name:      "common",
+		Help:      "common prometheus gauge",
+	}, []string{"host", "service", "metric"})
+	prometheusReg.Register(gau)
+	gPusher = push.New("http://127.0.0.1:9091", "demo-xxxx")
+	gPusher.Gatherer(prometheusReg)
+}
 
 func Push(metricItems []*dataobj.MetricValue) error {
 	var err error
@@ -27,51 +46,10 @@ func Push(metricItems []*dataobj.MetricValue) error {
 			// 如果数据有问题，直接跳过吧，比如mymon采集的到的数据，其实只有一个有问题，剩下的都没问题
 			continue
 		}
+		gau.WithLabelValues(item.Endpoint, item.Metric).Set(item.Value)
 	}
-
-	retry := 0
-	for len(metricItems) > 0 {
-		if retry >= 3 {
-			break
-		}
-		_, err := rpcCall()
-		if err != nil {
-			logger.Error(err)
-			retry += 1
-			time.Sleep(time.Millisecond * 500)
-			continue
-		}
-		return nil
-	}
-
+	err = gPusher.Push()
 	return err
-}
-
-func rpcCall() (dataobj.TransferResp, error) {
-	var reply dataobj.TransferResp
-	var err error
-	if true {
-		return reply, err
-	}
-
-	timeout := time.Duration(8) * time.Second
-	done := make(chan error, 1)
-
-	go func() {
-		done <- err
-	}()
-
-	select {
-	case <-time.After(timeout):
-		logger.Warningf("rpc call timeout, transfer addr\n")
-		return reply, fmt.Errorf("rpc call timeout")
-	case err := <-done:
-		if err != nil {
-			return reply, fmt.Errorf("rpc call done, but fail: %v", err)
-		}
-	}
-
-	return reply, nil
 }
 
 func CounterToGauge(item *dataobj.MetricValue) *dataobj.MetricValue {
